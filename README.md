462a081a5a6720448e914e790b400035e2669d22
Ramesh Choyal <krsna@Krsnas-MacBook-Air.local>
2024-07-29 12:37:43 +0530

    frontend desktop versionn

---
+++ README.md
@@ -0,0 +1,30 @@
+# React + Vite
+
+### Installation
+
+1.  **Clone the repository:**
+
+    ```bash
+    git clone  https://github.com/rameshchoyal/Frontend-desktop-react.git
+
+    ```
+
+2.  Cd to the repo
+
+    ```bash
+    cd Frontend-desktop-react
+
+    ```
+
+3.  Install the dependencies:
+
+    ```bash
+    npm install
+    ```
+
+4.  To start the development server, run:
+
+         ```bash
+         npm run dev
+
+    Open http://localhost:3000 with your browser to see the result.
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,27 @@
+import FertilityKit from "../components/FertilityKit";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Predict from "../components/Predict";
+import Question from "../components/Question";
+import Reviews from "../components/Reviews";
+import Steps from "../components/Steps";
+import WhyInito from "../components/WhyInito";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <FertilityKit />
+      <Predict />
+      <WhyInito />
+      <Steps />
+      <Reviews />
+      <Question />
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
